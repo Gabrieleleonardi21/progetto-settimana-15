bc3ab9208988cb5e2686b8a4da66f2c17629ab03
Gabrieleleonardi21 <leonardi.gabriele9821@gmail.com>
2026-08-28 09:20:09 +0200

    initial commit

---
+++ src/main/java/com/example/progettosettimana15/ProgettoSettimana15Application.java
@@ -0,0 +1,13 @@
+package com.example.progettosettimana15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSettimana15Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgettoSettimana15Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/progettosettimana15/ProgettoSettimana15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.progettosettimana15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSettimana15ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
